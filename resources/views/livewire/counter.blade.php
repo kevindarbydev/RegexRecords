9112bb20fa9e74ae38a06842aa64839f5440b766
Kevin <92401368+kevindarbydev@users.noreply.github.com>
2023-02-14 15:50:20 -0500

    new project with jetstream, counter component

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,8 @@
+<div style="text-align: center">
+    <h3>Hello World from Laravel + Jetstream + LiveWire </h3>
+    <hr />
+    <button wire:click="increment">+</button>
+    <h1>{{ $count }}</h1>
+</div>
+
+
