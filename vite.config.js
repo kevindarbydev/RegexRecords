48680f5b377e0f2bda00c0b295c8c0ebdb513c8c
Kevin <92401368+kevindarbydev@users.noreply.github.com>
2023-02-15 14:20:27 -0500

    clearing

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
-import vue from "vite-plugin-vue2";
-
-export default defineConfig({
-    plugins: [laravel(["resources/css/app.css", "resources/js/app.js"]), vue()],
-});
