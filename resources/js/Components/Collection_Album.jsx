2813dc1222f2d9b4f147f4a57ff8949da9f824fc
NatKwok <natkwok902@gmail.com>
2023-02-22 17:28:00 -0500

    Collection Album shows Yes or No Sale

--- resources/js/Components/Collection_Album.jsx
+++ resources/js/Components/Collection_Album.jsx
@@ -8,13 +8,25 @@ dayjs.extend(relativeTime);
 
 export default function Collection_Album({ collection_album }) {
 
+    function Sale (){
+
+    if(collection_album.for_sale == 0) {
+        console.log("For Sale");
+        return <h1>Yes</h1>;
+    }
+    else {
+        console.log("Not for Sale");
+        return <h1>No</h1>;
+    }
+}
 
     return (
         <div className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 m-6">
 
             <div className="flex flex-col w-64 justify-between p-4 leading-normal">
-
+               
                 <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                    
                     Album ID: {collection_album.album_id}
                 </p>
 
@@ -24,7 +36,8 @@ export default function Collection_Album({ collection_album }) {
                     </small>
                 </p>
                 <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                    For Sale? {collection_album.for_sale}
+                    {/* For Sale? {collection_album.for_sale} */}
+                    For Sale? <Sale/> 
                 </p>
 
             </div>
