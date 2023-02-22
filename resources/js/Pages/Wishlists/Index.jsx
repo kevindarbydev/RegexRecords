c19fba5dbd006366cc984f20fd688b69653f260d
NatKwok <natkwok902@gmail.com>
2023-02-22 09:21:14 -0500

    minor changes, doctrine/dbal install

--- resources/js/Pages/Wishlists/Index.jsx
+++ resources/js/Pages/Wishlists/Index.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import Wishlist from '@/Components/Wishlist';
-
 import InputError from '@/Components/InputError';
 import PrimaryButton from '@/Components/PrimaryButton';
 import { useForm, Head } from '@inertiajs/react';
+import DashboardTabs from '@/Components/Tabs/DashboardTabs';
  
 export default function Index({ auth, wishlists }) {
     const { data, setData, post, processing, reset, errors } = useForm({
@@ -18,8 +18,8 @@ export default function Index({ auth, wishlists }) {
  
     return (
         <AuthenticatedLayout auth={auth}>
+            <DashboardTabs/>
             <Head title="Create Wishlist" />
- 
             <div className="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
                 <form name="createForm" onSubmit={submit}>
                     <div className="flex flex-col">
