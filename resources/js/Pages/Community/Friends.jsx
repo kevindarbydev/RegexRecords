5410be232689512e6badf65ca7cb5bb28f26aba6
SeenaSK <104446906+SeenaSK@users.noreply.github.com>
2023-02-23 11:59:24 -0500

    sidebar transition working for friends page

--- resources/js/Pages/Community/Friends.jsx
+++ resources/js/Pages/Community/Friends.jsx
@@ -5,10 +5,16 @@ import { Head } from "@inertiajs/react";
 import Friendship from "./Partials/Friendship";
 import FriendshipPending from "./Partials/FriendshipPending";
 import CommunityTabs from "@/Layouts/Tabs/CommunityTabs";
-import { UsersIcon, ExclamationCircleIcon } from "@heroicons/react/24/solid";
+import {
+    UsersIcon,
+    ExclamationCircleIcon,
+    Bars3BottomLeftIcon,
+} from "@heroicons/react/24/solid";
+import { Transition } from "@headlessui/react";
 
 function Friends({ auth, currentFriendships, pendingFriendships }) {
     const [currentComp, setCurrentComp] = useState("friends");
+    const [isShowing, setIsShowing] = useState(true);
 
     const handleComponentChange = (newComp) => {
         setCurrentComp(newComp);
@@ -65,51 +71,66 @@ function Friends({ auth, currentFriendships, pendingFriendships }) {
                             ></path>
                         </svg>
                     </button> */}
-
-                    <aside
-                        id="default-sidebar"
-                        class="sticky top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
-                        aria-label="Sidebar"
+                    <button
+                        onClick={() => setIsShowing((isShowing) => !isShowing)}
+                        class="absolute z-50"
                     >
-                        <div class="h-full px-3 py-4 overflow-y-auto bg-violet-300 dark:bg-gray-800">
-                            <ul class="space-y-2">
-                                <li>
-                                    <a
-                                        href="#"
-                                        class={`flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 ${
-                                            currentComp === "friends"
-                                                ? "bg-white"
-                                                : ""
-                                        }`}
-                                        onClick={() =>
-                                            handleComponentChange("friends")
-                                        }
-                                    >
-                                        <UsersIcon className="h-6 w-6 text-blue-500" />
-                                        <span class="ml-3">Friends</span>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a
-                                        href="#"
-                                        class={`flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 ${
-                                            currentComp === "pending"
-                                                ? "bg-white"
-                                                : ""
-                                        }`}
-                                        onClick={() =>
-                                            handleComponentChange("pending")
-                                        }
-                                    >
-                                        <ExclamationCircleIcon className="h-6 w-6 text-blue-500" />
-                                        <span class="flex-1 ml-3 whitespace-nowrap">
-                                            Pending
-                                        </span>
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </aside>
+                        <Bars3BottomLeftIcon className="w-7 h-7" />
+                    </button>
+                    <Transition
+                        show={isShowing}
+                        enter="transition ease-in-out duration-300 transform"
+                        enterFrom="-translate-x-full"
+                        enterTo="translate-x-0"
+                        leave="transition ease-in-out duration-300 transform"
+                        leaveFrom="translate-x-0"
+                        leaveTo="-translate-x-full"
+                    >
+                        <aside
+                            id="default-sidebar"
+                            class="sticky top-0 left-0 z-40 sm:w-64 h-screen transition-transform sm:translate-x-0 "
+                            aria-label="Sidebar"
+                        >
+                            <div class="h-full px-3 py-4 overflow-y-auto bg-violet-300 dark:bg-gray-800">
+                                <ul class="space-y-2 mt-5">
+                                    <li>
+                                        <a
+                                            href="#"
+                                            class={`flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 ${
+                                                currentComp === "friends"
+                                                    ? "bg-white"
+                                                    : ""
+                                            }`}
+                                            onClick={() =>
+                                                handleComponentChange("friends")
+                                            }
+                                        >
+                                            <UsersIcon className="h-6 w-6 text-blue-500" />
+                                            <span class="ml-3">Friends</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a
+                                            href="#"
+                                            class={`flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 ${
+                                                currentComp === "pending"
+                                                    ? "bg-white"
+                                                    : ""
+                                            }`}
+                                            onClick={() =>
+                                                handleComponentChange("pending")
+                                            }
+                                        >
+                                            <ExclamationCircleIcon className="h-6 w-6 text-blue-500" />
+                                            <span class="flex-1 ml-3 whitespace-nowrap">
+                                                Pending
+                                            </span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </aside>
+                    </Transition>
                 </div>
                 {componentToShow}
             </div>
