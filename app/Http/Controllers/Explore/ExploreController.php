2baa54d5a896ca5fbb28e1a76a1bd9dbdf6a83c6
ChristopherPerrault <christopherperrault@hotmail.com>
2023-02-23 15:10:36 -0500

    pagination cont.

--- app/Http/Controllers/Explore/ExploreController.php
+++ app/Http/Controllers/Explore/ExploreController.php
@@ -4,15 +4,16 @@ namespace App\Http\Controllers\Explore;
 
 use App\Models\User;
 use Inertia\Inertia;
+use App\Models\Album;
 use Inertia\Response;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Pagination\Paginator;
-use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Facades\Auth;
 
-use App\Models\Album;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class ExploreController extends Controller
 {
@@ -25,21 +26,61 @@ class ExploreController extends Controller
     {
         $albums = Album::all();
         $totalAlbums = count($albums);
-        $perPage = 5;
+        // error_log('TOTAL ALBUMS: ' . $totalAlbums);
+        $perPage = 3;
         $page = Paginator::resolveCurrentPage('viewAllAlbums');
-        error_log('TOTAL ALBUMS: ' . $totalAlbums);
+        // $page = LengthAwarePaginator::resolveCurrentPage('viewAllAlbums');
+        // $page = Paginator::$defaultView;
+
+
+        //* LengthAwarePaginator arguments: (mixed $items, int $total, int $perPage, int|null $currentPage = null, array $options = [])
 
         $albums = new LengthAwarePaginator($albums->forPage($page, $perPage), $totalAlbums, $perPage, $page, [
             'path' => Paginator::resolveCurrentPath(),
             'pageName' => 'page',
         ]);
 
+        //* Paginator arguments:(mixed $items, int $perPage, int|null $currentPage = null, array $options = [])
 
+        // $albums = new Paginator($albums, $perPage, $page, [
+        //     'path' => Paginator::resolveCurrentPath(),
+        //     'pageName' => 'page',
+        // ]);
+
+        // $test = new LengthAwarePaginator($albums->forPage($page, $perPage), Album::all(), $perPage, $page, [
+        //     'path' => Paginator::resolveCurrentPath(),
+        //     'pageName' => 'page',
+        // ]);
+
+        // $albums = Album::all()->paginate(3);
+        // $albums = Album::paginate(3);
+        // $results = Album::paginate(3);
+        // $albums = Album::all();
+        // $input_array = array($albums);
+        // print_r(array_chunk($input_array, 3));
+        // $results = (array_chunk($input_array, 3, true));
+
+        // $ass = Album::all()->paginate(3);
 
         return Inertia::render('Explore/ViewAllAlbums', [
-            'albums' => Album::all(),
+            'perPage' => $perPage,
             'totalAlbums' => $totalAlbums,
-            compact('albums')
+
+            'albums' => Album::all(),
+            // 'albums' => $ass,
+
+            // 'albums' => Album::all(), $results,
+            // 'albums' => $albums,
+            // 'albums' => $results,
+            // 'albums' => $test,
+            // 'albums' => DB::table('albums')->paginate(3),
+            // 'albums' => DB::table('albums')->simplePaginate(3),
+            // 'albums' => Album::paginate(3),
+            // 'albums' => Album::simplePaginate(3),
+            // 'albums' => Album::all()->paginate(3),
+            // 'albums' => Album::all()->simplePaginate(3),
+            // 'albums' => Album::split()->simplePaginate(3),
+
 
         ]);
     }
