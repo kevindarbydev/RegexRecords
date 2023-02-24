313aafe08e375b7216cfff22821dbb0f1146e3b4
ChristopherPerrault <christopherperrault@hotmail.com>
2023-02-23 19:14:21 -0500

    explore section

--- resources/js/Pages/Explore/Index.jsx
+++ resources/js/Pages/Explore/Index.jsx
@@ -1,31 +1,25 @@
 import React from "react";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
-import Collection from "@/Pages/Dashboard/Partials/Collection";
-import InputError from "@/Components/InputError";
-import PrimaryButton from "@/Components/PrimaryButton";
-import { useForm, Head, Link } from "@inertiajs/react";
+import { Head, Link } from "@inertiajs/react";
 import ExploreTabs from "@/Layouts/Tabs/ExploreTabs";
-import ApplicationLogo from "@/Components/ApplicationLogo";
+import TopPicks from "./Partials/TopPicks";
+import NewReleases from "./Partials/NewReleases";
+import Featured from "./Partials/Featured";
 
 export default function Index({ auth }) {
     return (
         <AuthenticatedLayout auth={auth}>
             <Head title="Explore" />
             <ExploreTabs />
-            <h1 className="ml-2 mt-2 text-5xl font-normal hover:font-bold">
-                hoping to implement carousels here
-            </h1>
 
-            <h1 className="ml-2 mt-2 text-4xl">Music to your ears</h1>
-            <ApplicationLogo className=" w-40 h-40 animate-pulse rotate-180 hover:blur-lg" />
+            <h1 className="text-4xl m-4">Top Picks:</h1>
+            <TopPicks />
 
-            <h1 className="ml-2 mt-2 text-4xl">
-                Discover These Hot New Albums
-            </h1>
-            <ApplicationLogo className=" w-40 h-40 hover:animate-bounce" />
+            <h1 className="text-4xl m-4">Spotlight: "B" Artists</h1>
+            <Featured />
 
-            <h1 className="ml-2 mt-2 text-4xl">Regex Spotlight: "D" Artists</h1>
-            <ApplicationLogo className=" w-40 h-40 animate-spin-slow hover:animate-spin" />
+            <h1 className="text-4xl m-4">Fresh This Week:</h1>
+            <NewReleases />
         </AuthenticatedLayout>
     );
 }
