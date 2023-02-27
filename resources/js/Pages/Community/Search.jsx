1b65ad5f120ecccf3efd8dff4f423b21b9d55b16
SeenaSK <104446906+SeenaSK@users.noreply.github.com>
2023-02-27 11:28:17 -0500

    pagination implemented! (for searching users)

--- resources/js/Pages/Community/Search.jsx
+++ resources/js/Pages/Community/Search.jsx
@@ -4,18 +4,21 @@ import { Head } from "@inertiajs/react";
 import SearchBar from "./Partials/SearchBar";
 import SearchedUser from "./Partials/SearchedUser";
 import CommunityTabs from "@/Layouts/Tabs/CommunityTabs";
+import Pagination from "@/Components/Pagination";
 
 export default function Index({ auth, users }) {
+    console.log(users.links);
     return (
         <AuthenticatedLayout auth={auth}>
             <Head title="Search" />
             <CommunityTabs />
 
             <div className="flex flex-row flex-wrap m-10">
-                {users.map((user) => (
+                {users.data.map((user) => (
                     <SearchedUser key={user.id} user={user} />
                 ))}
             </div>
+            <Pagination links={users.links} />
         </AuthenticatedLayout>
     );
 }
