a647347c1f2b96b8d6d74738f669c0317965a8ab
NatKwok <natkwok902@gmail.com>
2023-02-28 12:20:40 -0500

    Order summary (total, subtotal, tax) shows in table

--- resources/js/Pages/Marketplace/Orders.jsx
+++ resources/js/Pages/Marketplace/Orders.jsx
@@ -4,15 +4,15 @@ import { useForm, Head } from "@inertiajs/react";
 
 import PrimaryButton from "@/Components/PrimaryButton";
 
-function subTotal(order_items) {
-    var quantity = order_items.quantity;
-    var itemPrice = order_items.price;
-    var total = quantity * itemPrice;
+// function subTotal(order_items) {
+//     var quantity = order_items.quantity;
+//     var itemPrice = order_items.price;
+//     var total = quantity * itemPrice;
 
-    console.log(quantity);
-}
+//     console.log(quantity);
+// }
 
-export default function Orders({ auth, order_items }) {
+export default function Orders({ auth, order_items, orders }) {
     return (
         <AuthenticatedLayout auth={auth}>
             <Head title="Orders" />
@@ -68,11 +68,47 @@ export default function Orders({ auth, order_items }) {
                         ))}
                     </table>
                 </div>
-                <div className="price">
+                <div className="flex justify-center items-center">
+                    <table className="w-1/2 text-sm text-center text-gray-500 dark:text-gray-400 mx-auto mt-5 bg-white">
+                        <thead className="text-xs text-gray-700 uppercase bg-gray-200 dark:bg-gray-700 dark:text-gray-400">
+                            <th scope="col" class="px-10 py-3">
+                                Ordered Date
+                            </th>
+                            <th scope="col" class="px-10 py-3">
+                                Subtotal
+                            </th>
+                            <th scope="col" class="px-10 py-3">
+                                Shipping
+                            </th>
+                            <th scope="col" class="px-10 py-3">
+                                Tax
+                            </th>
+                            <th scope="col" class="px-10 py-3">
+                                Total Price
+                            </th>
+                        </thead>
+                        {orders.map((order) => (
+                            <tbody>
+                                <tr>
+                                    <td>{order.created_at}</td>
+                                    <td>
+                                        {order.subtotal}
+                                    </td>
+                                    <td>{order.shipping}</td>
+                                    <td>{order.tax}</td>
+                                    <td>
+                                        {order.totalPrice}
+                                    </td>
+                                </tr>
+                            </tbody>
+                        ))}
+                    </table>
+                </div>
+                {/* <div className="price">
                     <button onClick={subTotal}>TEST</button>
 
                     <p>SubTotal:</p>
-                    <p id="subtotal"></p>
+                    <p id="subtotal">{orders.subtotal}</p>
 
                     <p>Taxes:</p>
                     <p id="taxes"></p>
@@ -82,7 +118,7 @@ export default function Orders({ auth, order_items }) {
 
                     <p>Final Total:</p>
                     <p id="finalTotal"></p>
-                </div>
+                </div> */}
                 <div className="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
                     <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
                         <PrimaryButton className="mt-4">
