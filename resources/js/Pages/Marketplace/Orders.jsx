b182ccb4525702f78466010403e717acb794ba21
NatKwok <natkwok902@gmail.com>
2023-02-28 12:07:39 -0500

    JS functions, and working on showing total, subtotal, tax, etc

--- resources/js/Pages/Marketplace/Orders.jsx
+++ resources/js/Pages/Marketplace/Orders.jsx
@@ -4,6 +4,14 @@ import { useForm, Head } from "@inertiajs/react";
 
 import PrimaryButton from "@/Components/PrimaryButton";
 
+function subTotal(order_items) {
+    var quantity = order_items.quantity;
+    var itemPrice = order_items.price;
+    var total = quantity * itemPrice;
+
+    console.log(quantity);
+}
+
 export default function Orders({ auth, order_items }) {
     return (
         <AuthenticatedLayout auth={auth}>
@@ -43,8 +51,8 @@ export default function Orders({ auth, order_items }) {
                                             className="w-24 h-24 rounded-lg object-cover mr-8 mt-2 mx-auto"
                                         />
                                     </td>
-                                    <td>{item.price}</td>
-                                    <td>{item.quantity}</td>
+                                    <td id="price">{item.price}</td>
+                                    <td id="quantity">{item.quantity}</td>
                                     <td>
                                         {/* <a href={route('orders.destroy', chirp.id)}></a> */}
                                         <PrimaryButton className="mt-2">
@@ -59,9 +67,21 @@ export default function Orders({ auth, order_items }) {
                             </tbody>
                         ))}
                     </table>
-                    {/* {order_items.map(item =>
-                        <Order_Item key={item.id} order_item={item} />
-                    )} */}
+                </div>
+                <div className="price">
+                    <button onClick={subTotal}>TEST</button>
+
+                    <p>SubTotal:</p>
+                    <p id="subtotal"></p>
+
+                    <p>Taxes:</p>
+                    <p id="taxes"></p>
+
+                    <p>Shipping:</p>
+                    <p id="shipping"></p>
+
+                    <p>Final Total:</p>
+                    <p id="finalTotal"></p>
                 </div>
                 <div className="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
                     <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
