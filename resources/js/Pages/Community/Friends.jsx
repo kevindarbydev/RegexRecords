9d3aa9f52363223eb3eb4395a612cd8ea0d116d4
Kevin <92401368+kevindarbydev@users.noreply.github.com>
2023-03-03 11:42:58 -0500

    reverted changes

--- resources/js/Pages/Community/Friends.jsx
+++ resources/js/Pages/Community/Friends.jsx
@@ -48,7 +48,6 @@ function Friends({
     } else if (currentComp === "pending") {
         console.log("pending");
 
-
         console.log("current friends");
         console.dir(currentFriendships);
 
@@ -56,16 +55,18 @@ function Friends({
         console.dir(pendingFriendships);
         componentToShow = (
             <div className="flex flex-row flex-wrap m-10">
-                {/* {pendingFriendships.map((friendship) =>
+                {pendingFriendships.map((friendship) =>
                     // FIXME: works for now, but will try and set FK relationship in DB and set friendships to cascade delete on user delete
-                    friendship.sender !== null &&
-                    friendship.recipient !== null ? (
+                    friendship.sender == null ||
+                    friendship.recipient == null ? (
+                        <></>
+                    ) : (
                         <FriendshipPending
                             key={friendship.id}
                             friendship={friendship}
                         />
-                    ) : null
-                )} */}
+                    )
+                )}
             </div>
         );
         console.log("if it didnt crash, problem found");
--- resources/js/Pages/Community/Partials/FriendshipPending.jsx
+++ resources/js/Pages/Community/Partials/FriendshipPending.jsx
@@ -30,7 +30,7 @@ function FriendshipPending({ friendship }) {
         <div class="mr-5">
             <Toaster />
             <div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                {friendship.recipient_id == user.id ? (
+                {friendship.recipient.id == user.id ? (
                     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                         {friendship.sender.name}
                     </h5>
@@ -48,7 +48,7 @@ function FriendshipPending({ friendship }) {
                 </p>
                 <div className="flex flex-row">
                     <form onSubmit={acceptFriendship}>
-                        {friendship.recipient_id == user.id ? (
+                        {friendship.recipient.id == user.id ? (
                             <div className="flex flex-row self-center mt-5">
                                 <UserPlusIcon class="w-5 h-5 self-center" />
                                 <PrimaryButton
@@ -63,7 +63,7 @@ function FriendshipPending({ friendship }) {
                         )}
                     </form>
                     <form onSubmit={denyFriendship}>
-                        {friendship.recipient_id == user.id ? (
+                        {friendship.recipient.id == user.id ? (
                             <div className="flex flex-row self-center mt-5">
                                 <PrimaryButton
                                     onClick={notifyDeny}
