ed7af993c74a912da396f5417e7976fd7b2b76b7
Kevin <92401368+kevindarbydev@users.noreply.github.com>
2023-03-03 10:40:48 -0500

    trying to fix friendshippending crashing

--- resources/js/Pages/Community/Friends.jsx
+++ resources/js/Pages/Community/Friends.jsx
@@ -56,12 +56,10 @@ function Friends({
         console.dir(pendingFriendships);
         componentToShow = (
             <div className="flex flex-row flex-wrap m-10">
-
                 {pendingFriendships.map((friendship) =>
-                
                     // FIXME: works for now, but will try and set FK relationship in DB and set friendships to cascade delete on user delete
-                    friendship.sender !== null &&
-                    friendship.recipient !== null ? (
+                    friendship.sender == null ||
+                    friendship.recipient == null ? (
                         <FriendshipPending
                             key={friendship.id}
                             friendship={friendship}
