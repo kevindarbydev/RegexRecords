3bafcc4f940deeb9e87826c3d4ee07889383a7af
Kevin <92401368+kevindarbydev@users.noreply.github.com>
2023-03-06 14:33:41 -0500

    modernized convo list

--- resources/js/Pages/Messages/Index.jsx
+++ resources/js/Pages/Messages/Index.jsx
@@ -71,7 +71,7 @@ function Index({
                 {success && (
                     <div className="alert alert-success">{success}</div>
                 )}
-                <div className="w-1/2 lg:w-1/4 h-1/2 bg-gray-200 px-4 py-8 ">
+                <div className="w-3/4 lg:w-1/4 h-fit bg-gray-200 px-4 py-8 ">
                     <h1
                         className="font-medium text-lg mb-4"
                         style={{ display: "flex", alignItems: "center" }}
