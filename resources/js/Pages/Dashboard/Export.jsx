46057df8a1dad282edc4b494c74b538eaebbcfb4
Kevin <92401368+kevindarbydev@users.noreply.github.com>
2023-03-07 16:40:56 -0500

    zzz

--- resources/js/Pages/Dashboard/Export.jsx
+++ resources/js/Pages/Dashboard/Export.jsx
@@ -57,7 +57,7 @@ export default function Index({ auth, cartCount, downloadUrl, fileName, headers 
             {downloadUrl && (
                 <div>
                     <a
-                        href={`${window.location.origin}/storage/downloads/${fileName}`}
+                        href={`${window.location.origin}/storage/app/downloads/${fileName}`}
                         target="_blank"
                         rel="noreferrer"
                         download={fileName}
