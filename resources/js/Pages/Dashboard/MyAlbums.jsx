396d0544fbcad151cb2530868178c5321e7b7cde
ChristopherPerrault <christopherperrault@hotmail.com>
2023-03-06 20:09:31 -0500

    dashboard/albums styled

--- resources/js/Pages/Dashboard/MyAlbums.jsx
+++ resources/js/Pages/Dashboard/MyAlbums.jsx
@@ -109,85 +109,93 @@ export default function Index({
         <AuthenticatedLayout auth={auth} cartCount={cartCount}>
             <Head title="Albums" />
             <DashboardTabs />
-            <div className="flex flex-row">
-                <div className="p-4 sm:p-6 lg:p-8 ml-10">
-                    <div className="relative">
-                        <form className="w-3/4 md:w-1/5 m-6" onSubmit={submit}>
-                            <label htmlFor="AlbumName">Album Name</label>
-                            <input
-                                name="AlbumName"
-                                value={data.album_name}
-                                placeholder="Dark Side of the Moon"
-                                className="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
-                                onChange={(e) => {
-                                    setData("album_name", e.target.value);
-                                    handleSearchQueryChange(e);
-                                }}
-                            />
-                            <InputError
-                                message={errors.album_name}
-                                className="mt-2"
-                            />
-                            <label htmlFor="artist">Artist</label>
-                            <input
-                                name="artist"
-                                value={data.artist}
-                                placeholder="Pink Floyd"
-                                className="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
-                                onChange={(e) => {
-                                    setData("artist", e.target.value);
-                                    handleSearchQueryChange(e);
-                                }}
-                                list="albumList"
-                            />
-                            <InputError
-                                message={errors.artist}
-                                className="mt-2"
-                            />
-                            {/* Search result menu */}
-                            {searchResults &&
-                                searchResults.length > 0 &&
-                                showDropdown && (
-                                    <div className="absolute z-10 w-full md:w-1/5 mt-2 rounded-md shadow-lg">
-                                        <div className="bg-white rounded-md shadow-xs">
-                                            <ul className="py-1 overflow-auto text-base leading-6 rounded-md shadow-xs max-h-32">
-                                                {uniqueTitles.map((result) => (
-                                                    <li
-                                                        key={result.id}
-                                                        className="cursor-pointer text-gray-900 hover:bg-indigo-400 hover:text-white py-2 px-3"
-                                                        onClick={() =>
-                                                            handleDropdownItemClick(
-                                                                result
-                                                            )
-                                                        }
-                                                    >
-                                                        {result?.title}
-                                                    </li>
-                                                ))}
-                                            </ul>
-                                        </div>
-                                    </div>
-                                )}
-
+            <div className="flex flex-row m-8 mb-20">
+                {/* top left container (form)*/}
+                <div className="w-2/4">
+                    <form className="" onSubmit={submit}>
+                        <label htmlFor="AlbumName">Album Name</label>
+                        <input
+                            name="AlbumName"
+                            value={data.album_name}
+                            placeholder="Dark Side of the Moon"
+                            className="block w-4/6 border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm p-2 mb-2"
+                            onChange={(e) => {
+                                setData("album_name", e.target.value);
+                                handleSearchQueryChange(e);
+                            }}
+                        />
+                        <InputError
+                            message={errors.album_name}
+                            className="mt-2"
+                        />
+                        <label htmlFor="artist">Artist</label>
+                        <input
+                            name="artist"
+                            value={data.artist}
+                            placeholder="Pink Floyd"
+                            className="block w-4/6 border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm p-2"
+                            onChange={(e) => {
+                                setData("artist", e.target.value);
+                                handleSearchQueryChange(e);
+                            }}
+                            list="albumList"
+                        />
+                        <InputError message={errors.artist} className="mt-2" />
+                        <div className="relative">
                             <PrimaryButton
-                                className="mt-4"
+                                className="mt-4 mr-6 absolute right-64"
                                 processing={processing}
                             >
                                 Post Album
                             </PrimaryButton>
-                        </form>
-                    </div>
-                    <div className="flex flex-col md:flex-row flex-wrap">
-                        {albums.map((album) => (
-                            <Album
-                                key={album.id}
-                                album={album}
-                                collections={collections}
-                                albumsWithRatings={albumsWithRatings}
-                            />
-                        ))}
-                    </div>
+                        </div>
+                        {/* Search result menu */}
+                        {searchResults &&
+                            searchResults.length > 0 &&
+                            showDropdown && (
+                                <div className="absolute z-10 w-full md:w-1/5 mt-2 rounded-md shadow-lg">
+                                    <div className="bg-white rounded-md shadow-xs">
+                                        <ul className="py-1 overflow-auto text-base leading-6 rounded-md shadow-xs max-h-32">
+                                            {uniqueTitles.map((result) => (
+                                                <li
+                                                    key={result.id}
+                                                    className="cursor-pointer text-gray-900 hover:bg-indigo-400 hover:text-white py-2 px-3"
+                                                    onClick={() =>
+                                                        handleDropdownItemClick(
+                                                            result
+                                                        )
+                                                    }
+                                                >
+                                                    {result?.title}
+                                                </li>
+                                            ))}
+                                        </ul>
+                                    </div>
+                                </div>
+                            )}
+                    </form>
                 </div>
+                {/* top right container (banner) */}
+                <div className="dashboard-albums-right w-2/4 mr-32">
+                    <h1 className="text-6xl flex justify-end ">
+                        Add new albums.
+                    </h1>
+                    <h1 className="text-6xl flex justify-end ">
+                        See your stash.
+                    </h1>
+                    <h1 className="text-6xl flex justify-end ">♾ Repeat.</h1>
+                </div>
+            </div>
+            {/* bottom container (albums) */}
+            <div className="dashboard-albums-bottom flex flex-col md:flex-row flex-wrap">
+                {albums.map((album) => (
+                    <Album
+                        key={album.id}
+                        album={album}
+                        collections={collections}
+                        albumsWithRatings={albumsWithRatings}
+                    />
+                ))}
             </div>
         </AuthenticatedLayout>
     );
