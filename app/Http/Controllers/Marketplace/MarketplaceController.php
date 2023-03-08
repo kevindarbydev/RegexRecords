e576e278aa318627ee870c4b8c1aec7c073cf650
Kevin <92401368+kevindarbydev@users.noreply.github.com>
2023-03-08 09:01:17 -0500

    trying to fix error

--- app/Http/Controllers/Marketplace/MarketplaceController.php
+++ app/Http/Controllers/Marketplace/MarketplaceController.php
@@ -38,11 +38,10 @@ class MarketplaceController extends Controller
 
         return Inertia::render('Marketplace/Index', [
             'current_user' => Auth::user(),
-            'collection_albums' => Collection_Album::where('for_sale', true)->latest()->groupBy('album_id')->get(),
-            'collection_albums2' => DB::table('collection__albums')->where('for_sale', true)->join('albums', 'collection__albums.album_id', '=', 'albums.id')->orderBy('albums.value', 'asc')->groupBy('album_id')->get(),
-            'collection_albums3' => DB::table('collection__albums')->where('for_sale', true)->join('albums', 'collection__albums.album_id', '=', 'albums.id')->orderBy('albums.value', 'desc')->groupBy('album_id')->get(),
-            'collection_albums4' => DB::table('collection__albums')->where('for_sale', true)->join('collections', 'collection__albums.collection_id', '=', 'collections.id')->join('users', 'collections.user_id', '=', 'users.id')->orderBy('users.name', 'asc')->groupBy('album_id')->get(),
-        
+            'collection_albums' => Collection_Album::where('for_sale', true)->latest()->get(),
+            'collection_albums2' => DB::table('collection__albums')->where('for_sale', true)->join('albums', 'collection__albums.album_id', '=', 'albums.id')->orderBy('albums.value', 'asc')->get(),
+            'collection_albums3' => DB::table('collection__albums')->where('for_sale', true)->join('albums', 'collection__albums.album_id', '=', 'albums.id')->orderBy('albums.value', 'desc')->get(),
+            'collection_albums4' => DB::table('collection__albums')->where('for_sale', true)->join('collections', 'collection__albums.collection_id', '=', 'collections.id')->join('users', 'collections.user_id', '=', 'users.id')->orderBy('users.name', 'asc')->get(),
             'albums' => Album::with('user')->latest()->get(),
             'collections' => Collection::with('user')->latest()->get(),
             'users' => User::all(),
