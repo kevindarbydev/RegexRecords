074a5db4a0ab746f41f8223e5a8298f61bff3271
Kevin <92401368+kevindarbydev@users.noreply.github.com>
2023-03-14 08:47:13 -0400

    json data type error

--- database/migrations/2023_02_16_142346_create_albums_table.php
+++ database/migrations/2023_02_16_142346_create_albums_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('album_name');
             $table->string('artist');
             $table->integer('value');
-            $table->json('subgenres');
+            $table->text('subgenres')->nullable();
             $table->timestamps();
         });
     }
